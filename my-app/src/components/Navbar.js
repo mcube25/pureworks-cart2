dd10f53b39ada004e9d608a1d19b2b8cf9eeba7f
mcube25 <mcube3567@gmail.com>
2021-05-29 02:35:37 -0700

    add style

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo512.png';
+import styled from 'styled-components'
 
 export default class Navbar extends Component {
     render() {
@@ -17,12 +18,34 @@ export default class Navbar extends Component {
                  </Link>
              </ul>
              <Link to='/cart' className="ml-auto">
-                 <button>
+                 <ButtonContainer>
+                     <span className="mr-2">
                      <i className="fas fa-cart-plus"/>
+                     </span>
                      my cart
-                 </button>
+                 </ButtonContainer>
              </Link>
             </nav>
         )
     }
 }
+
+const ButtonContainer = styled.button`
+text-transform: capitalize;
+font-size: 1.4rem;
+background: transparent;
+border: 0.05rem solid var(--lightBlue);
+color: var(--lightBlue);
+border- radius:0.5rem;
+padding: 0.2rem 0.5rem;
+cursor: pointer;
+margin: 0.2rem 0.5rem;
+transition: all 0.5s ease-in-out;
+&:hover {
+    background:var(--lightBlue);
+    color: var(--mainBlue);
+}
+&:focus {
+    outline: none;
+}
+`
