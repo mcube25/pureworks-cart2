074c392ba0477de37c87245399a8cca5e648a57c
mcube25 <mcube3567@gmail.com>
2021-05-31 13:46:56 -0700

    styling navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo512.png';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
+
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
              <Link to="/">
 <img src={logo} alt="store" className="navbar-brand"/>
              </Link>
@@ -25,27 +27,16 @@ export default class Navbar extends Component {
                      my cart
                  </ButtonContainer>
              </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
 
-const ButtonContainer = styled.button`
-text-transform: capitalize;
-font-size: 1.4rem;
-background: transparent;
-border: 0.05rem solid var(--lightBlue);
-color: var(--lightBlue);
-border- radius:0.5rem;
-padding: 0.2rem 0.5rem;
-cursor: pointer;
-margin: 0.2rem 0.5rem;
-transition: all 0.5s ease-in-out;
-&:hover {
-    background:var(--lightBlue);
-    color: var(--mainBlue);
-}
-&:focus {
-    outline: none;
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color: var(--mainWhite) !important;
+    font-size: 0.3rem;
+    text-transform: capitalize !important;
 }
 `
