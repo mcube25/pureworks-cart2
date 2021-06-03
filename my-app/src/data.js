cf1a36bdea1717de102c3b3f04904ef216e04871
mcube25 <mcube3567@gmail.com>
2021-06-03 02:15:41 -0700

    added store data

---
+++ my-app/src/data.js
@@ -0,0 +1,68 @@
+export const storeProducts = [
+    {
+        id:1,
+        title: "Google Pixel - Black",
+        img: "img/product-1.png",
+        price: 10,
+        company: "Google",
+        info: "lorem ipsum dolor",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id:2,
+        title: "samsung gallaxy",
+        img: "img/product-1.png",
+        price: 10,
+        company: "Google",
+        info: "lorem ipsum dolor",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id:3,
+        title: "Nokia phantrom",
+        img: "img/product-1.png",
+        price: 10,
+        company: "Google",
+        info: "lorem ipsum dolor",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id:4,
+        title: "Apple7",
+        img: "img/product-1.png",
+        price: 10,
+        company: "Google",
+        info: "lorem ipsum dolor",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id:5,
+        title: "lenovo top 3",
+        img: "img/product-1.png",
+        price: 10,
+        company: "Google",
+        info: "lorem ipsum dolor",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id:6,
+        title: "Gren",
+        img: "img/product-1.png",
+        price: 10,
+        company: "Google",
+        info: "lorem ipsum dolor",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+]
