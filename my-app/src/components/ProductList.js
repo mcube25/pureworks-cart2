495426100edf99c77b4cf2a40f29ca4dad97ed0f
mcube25 <mcube3567@gmail.com>
2021-06-08 15:59:51 -0700

    product details

--- my-app/src/components/ProductList.js
+++ my-app/src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
 import {storeProducts} from '../data';
+import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
     state={
@@ -9,14 +10,22 @@ products: storeProducts
     }
 
     render() {
-        console.log(this.state.products);
         return (
             <React.Fragment>
                 <div className="py-5">
                 <div className="container">
                     <Title name="our" title="products"/>
                 <div className="row">
-            
+            <ProductConsumer>
+                {
+                    (value)=>{
+          console.log(value);
+          return value.products.map(product =>{
+              return <Product key={product.id} product={product}/>
+          })
+                    }
+                }
+            </ProductConsumer>
                 </div>
                 </div>
                 </div>
