e280599436c0d0c033eac8f3a24c1e6a3efd4109
mcube25 <mcube3567@gmail.com>
2021-06-08 16:02:08 -0700

    product provider added to index

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {ProductProvider} from './context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
     <Router>
     <App />
     </Router>
-  </React.StrictMode>,
+  </ProductProvider>,
+  
   document.getElementById('root')
 );
 
