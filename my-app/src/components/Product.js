918dc49ccb7ec6936b7cad18e0b425a77b2b6d3d
mcube25 <mcube3567@gmail.com>
2021-06-10 12:54:33 -0700

    add shop cart

--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -9,10 +9,12 @@ export default class Product extends Component {
         return (
             <ProductWrapper className= "col-9 mx-auto col-md-6 col-lg-3 my-3">
                <div className="card">
-<div className = "img-container p-5" onClick={console.log('hi u clicked on me img')}>
+<div className = "img-container p-5" onClick={()=> console.log('hi u clicked on me img')}>
 <Link to="/details">
     <img src={img} alt="product img" className="card-img-top"/>
 </Link>
+<button className="cart-btn" disabled={inCart?true:false} onClick={()=>{console.log('add to the cart')}}/>
+{inCart?(<p className="text-capitalize mb-0" disabled>inCart</p>):<i className="fas fa-cart-plus"/>}
 </div>
                </div>
             </ProductWrapper >
