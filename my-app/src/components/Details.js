2ec78064245afedc0a291fd5dcb39d65dea42d34
mcube25 <mcube3567@gmail.com>
2021-06-14 16:31:53 -0700

    set up details page

--- my-app/src/components/Details.js
+++ my-app/src/components/Details.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h3>hello from details</h3>
-            </div>
+           <ProductConsumer>
+               {(value)=>{
+          const {id,company,img,info, price,title,inCart} = value.detailProduct;
+          return (
+              <div className="container py-5">
+               {/* title */}
+               <div className="row">
+                   <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                   <h1>{title}</h1>
+                   </div>
+               </div>
+               {/* end of title */}
+                {/* product info  */}
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-6 my-3">
+                        <img src={img} className="img-fluid" alt="product"/>
+                        </div>
+                        {/*product text */}
+                    <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                        <h1>model: {title}</h1>
+                        <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                            made by : <span>{company}</span>
+                        </h4>
+                        <h4>
+                            <strong>price :<span>$</span>{price}</strong>
+                        </h4>
+                        <p className="font-weight-bold mt-3 mb-0">
+                            some info about the product : 
+                        </p>
+                        <p className="text-muted">{info}</p>
+                        {/*button */}
+                        <div>
+                            <Link to='/'>
+                                <ButtonContainer>
+                                    Back to Products
+                                </ButtonContainer>
+                            </Link>
+                            <ButtonContainer
+                            disabled={inCart?true:false}
+                            onClick={()=>{
+                                value.addToCart(id)
+                            }}
+                            >
+                                {inCart?'inCart':'add to Cart'}
+                            </ButtonContainer>
+                        </div>
+                    </div>
+                </div>
+              </div>
+          )
+               }}
+           </ProductConsumer>
         )
     }
 }
