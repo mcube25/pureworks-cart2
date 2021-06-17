f58ab1bc4eb26313aa1f8b6c34e0dbf9703b3555
mcube25 <mcube3567@gmail.com>
2021-06-17 16:56:43 -0700

    added open modal to cart

--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -19,7 +19,9 @@ export default class Product extends Component {
                                     <img src={img} alt="product img" className="card-img-top" />
                                 </Link>
                                 <button className="cart-btn" disabled={inCart ? true : false} onClick={() => 
-                                    { value.addToCart(id); }}>
+                                    { value.addToCart(id); 
+                                    value.openModal(id);
+                                    }}>
                                     {inCart ? (<p className="text-capitalize mb-0" disabled>
                                         {" "}
                                                  inCart</p>) : <i className="fas fa-cart-plus" />}
