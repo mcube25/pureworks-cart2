ef5290269efc79d5dd8e3a576a70a28ac05a5dea
mcube25 <mcube3567@gmail.com>
2021-06-17 17:04:34 -0700

    added modal to details page

--- my-app/src/components/Details.js
+++ my-app/src/components/Details.js
@@ -46,7 +46,8 @@ export default class Details extends Component {
                             <ButtonContainer cart
                             disabled={inCart?true:false}
                             onClick={()=>{
-                                value.addToCart(id)
+                                value.addToCart(id);
+                                value.openModal(id);
                             }}
                             >
                                 {inCart?'inCart':'add to Cart'}
