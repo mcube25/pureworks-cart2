85aaf275dc8908fe4838760a9396b9a143c88b00
mcube25 <mcube3567@gmail.com>
2021-06-23 20:03:53 -0700

    empty cart component

---
+++ my-app/src/components/Cart/EmptyCart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function EmptyCart() {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-10 mx-auto text-center text-title"><h3>your cart is currently empty</h3></div>
+            </div>
+        </div>
+    )
+}
+//4:28
