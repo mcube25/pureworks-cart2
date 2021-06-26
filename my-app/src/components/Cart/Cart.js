d98c63f79942a0262a5819b9d15ba82ed1829354
mcube25 <mcube3567@gmail.com>
2021-06-25 19:36:30 -0700

    cart

--- my-app/src/components/Cart/Cart.js
+++ my-app/src/components/Cart/Cart.js
@@ -3,6 +3,8 @@ import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
 import { ProductConsumer } from '../../context';
+import CartList from './Cartlist';
+import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
     render() {
@@ -16,6 +18,8 @@ export default class Cart extends Component {
                                 <React.Fragment>
                                     <Title name="your" title="cart" />
                                     <CartColumns />
+                                    <CartList value={value}/>
+                                    <CartTotals value={value}/>
                                 </React.Fragment>
                             );
                         } else {
---
+++ my-app/src/components/Cart/CartTotals.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CartTotals({value}) {
+    const {cartSubTotal,cartTax,cartTotal}
+    return (
+        <div>
+            
+        </div>
+    )
+}
