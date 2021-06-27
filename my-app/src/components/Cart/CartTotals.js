ce17e37f6a78d3d9c25bddc7d15b804d4c982712
mcube25 <mcube3567@gmail.com>
2021-06-27 15:52:00 -0700

    added total function

--- my-app/src/components/Cart/CartTotals.js
+++ my-app/src/components/Cart/CartTotals.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function CartTotals({value}) {
-    const {cartSubTotal,cartTax,cartTotal}
-    return (
-        <div>
-            
+export default function CartTotals({ value }) {
+    const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+    return
+    <React.Fragment>
+        <div className="container">
+            <div className="row">
+                <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-right">
+                    <Link to="/">
+                        <button className="btn btn-outline-danger mb-3 px-5" type="button"
+                        onClick={()=>clearCart()}>
+                            clear cart</button></Link>
+                            <h5><span className="text-title"> subtotal : </span>
+                            <strong>$ {cartSubTotal}</strong></h5>
+                            <h5><span className="text-title"> Tax : </span>
+                            <strong>$ {cartTax}</strong></h5>
+                            <h5><span className="text-title"> Total : </span>
+                            <strong>$ {cartTotal}</strong></h5>
+                </div>
+            </div>
         </div>
-    )
+    </React.Fragment>
 }
